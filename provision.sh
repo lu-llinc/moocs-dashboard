4ba294d79dab686ff8e9e2560c943589f023e570
Jasper Ginn <jasperginn@hotmail.com>
2016-05-12 15:30:43 +0200

    Removed second wget in provisioner

--- provision.sh
+++ provision.sh
@@ -73,7 +73,7 @@ cd /vagrant/downloads
 
 if [ ! -f $shiny ]; then
 	echo "Downloading shiny installer..."
-    wget wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.2.786-amd64.deb
+    wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.2.786-amd64.deb
     chmod +x $shiny
 fi
 
